07d15ed20f41d1a1693458c8b26f71e734817c40
Dave Kokandy <drkokandy@gmail.com>
2017-11-11 06:20:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ function findMatching(drivers, string){
     return name.toUpperCase() === string.toUpperCase();
   });
 }
+
+function fuzzyMatch(drivers, string) {
+  return drivers.filter(function(name) {
+    return name.slice(0,3) === string.slice(0.3);
+  });
+}
